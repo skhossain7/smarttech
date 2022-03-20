7864a86a7e4e326ebd4328c21b6d0b108f2e0072
Mohammed <Mohammed@DESKTOP-5O5LHSL.lan>
2022-03-20 09:32:12 -0400

    First Try

---
+++ SmartTech-24/src/test/java/com/mohammed/First_try.java
@@ -0,0 +1,12 @@
+package com.mohammed;
+
+public class First_try {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//System.out.println();  = syso ctrl+space 
+	//	System.out.println("Hello Java");
+System.out.println("My first Java program");
+	}
+
+}
